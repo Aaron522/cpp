4b679eb44278e41509844075e672f8621d318466
pugugugu2 <gurpreet3@hotmail.co.uk>
2020-11-21 00:25:13 +0000

    e

--- project.cpp
+++ project.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+using namespace std;
+using std::cin;
 int main()
 {
-	std::cout << "Aanya is fattythereker123";
-	return 0;
-}
+	cout << "How old are you?";
+	int e1;
+	cin >> e1;
+	cout << "You are " << e1 << " Years old.";
+} 
